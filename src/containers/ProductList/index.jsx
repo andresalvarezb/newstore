9a0bfe0338c2d10b704d52b146f437bf35c3481f
Andres Alvarez <andres.alvarez094@hotmail.es>
2022-05-23 09:10:00 -0500

    fix imports and style classes

---
+++ src/containers/ProductList/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './productList.module.css';
+import useFetchData from '@hooks/useFetchData';
+import ProductItem from '@components/ProductItem';
+
+// TODO: it's important understand how work the API to use it
+const API = 'https://api.escuelajs.co/api/v1/products';
+
+// `${API}?limit=${12}&offset=${0}`
+
+// ? Stateful
+function ProductList() {
+	const products = useFetchData(API);
+
+	return (
+		<section className={styles['main-container']}>
+			<div className={styles['ProductList']}>
+				{products.map((product) => (
+					<ProductItem key={product.id} product={product} />
+				))}
+			</div>
+		</section>
+	);
+}
+
+export default ProductList;
